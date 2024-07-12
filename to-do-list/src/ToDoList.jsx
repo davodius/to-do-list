8fc09c225ce97b71d4be1156c71ed51b476e80b3
davodius <davidhanebrant@gmail.com>
2024-07-12 17:38:13 +0200

    first commit

---
+++ to-do-list/src/ToDoList.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newtask, setnewtask] = useState("");
+
+  function handleInputchange() {}
+
+  return <></>;
+}
+export default ToDoList;
