394416cd644d4b1ed51776ec5ee60fbd224d3902
davodius <davidhanebrant@gmail.com>
2024-07-13 18:09:10 +0200

    add works not delete

--- to-do-list/src/ToDoList.jsx
+++ to-do-list/src/ToDoList.jsx
@@ -1,11 +1,69 @@
 import React, { useState } from "react";
 
 function ToDoList() {
-  const [tasks, setTasks] = useState([]);
-  const [newtask, setnewtask] = useState("");
+  const [tasks, setTasks] = useState([
+    "eat breakfast ",
+    "take a shower ",
+    "walk the dog",
+  ]);
+  const [newTask, setNewTask] = useState("");
 
-  function handleInputchange() {}
+  function handleInputchange(event) {
+    setNewTask(event.target.value);
+  }
 
-  return <></>;
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function moveTaskUp(index) {}
+
+  function moveTaskDown(index) {}
+
+  return (
+    <div className="to-do-list">
+      <h1>To-Do-List</h1>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newtask}
+          onChange={handleInputchange}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+
+      <ol>
+        {tasks.map((task, index) => (
+          //
+          <li key={index}>
+            <span className="text">{task}</span>
+
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+            <button className="move-button" onClick={() => moveTaskUp(index)}>
+              Up
+            </button>
+            <button className="move-button" onClick={() => moveTaskDown(index)}>
+              Down
+            </button>
+          </li>
+          //
+        ))}
+      </ol>
+    </div>
+  );
 }
 export default ToDoList;
